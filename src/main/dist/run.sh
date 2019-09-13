70addd97cc141921e00c74b6560a96c7906abaab
Ellanki <sellanki@mcw.edu>
2019-09-13 13:51:06 -0500

    run.sh

---
+++ src/main/dist/run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+. /etc/profile
+
+SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
+APPNAME=EnsemblDataPipeline
+APPDIR=/home/rgddata/pipelines/$APPNAME
+EMAILLIST=selanki@mcw.edu,mtutaj@mcw.edu
+
+if [ "$SERVER" == "REED" ]; then
+  EMAILLIST=jrsmith@mcw.edu,selanki@mcw.edu,mtutaj@mcw.edu
+fi
+
+cd $APPDIR
+
+java -Dspring.config=$APPDIR/../properties/default_db.xml \
+    -Dlog4j.configuration=file://$APPDIR/properties/log4j.properties \
+    -jar lib/$APPNAME.jar "$@" 2>&1
+
+mailx -s "[$SERVER] Ensembl Data Pipeline" $EMAILLIST < $APPDIR/logs/status.log
