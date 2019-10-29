ef133b55932d9d07d0b95665686ad57083bb3e77
Ellanki <sellanki@mcw.edu>
2019-10-29 15:53:25 -0500

    run.sh

--- src/main/dist/run.sh
+++ src/main/dist/run.sh
@@ -14,6 +14,6 @@ cd $APPDIR
 
 java -jar -Dspring.config=$APPDIR/../properties/default_db.xml \
     -Dlog4j.configuration=file://$APPDIR/properties/log4j.properties \
-    -jar lib/$APPNAME.jar "$@" 2>&1
+    -jar lib/$APPNAME.jar "$@" status.log 2>&1
 
 mailx -s "[$SERVER] EnsemblDataPipeline OK!" $EMAILLIST < $APPDIR/logs/status.log
