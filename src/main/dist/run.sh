b4558434fac2adb0e08e3006991739ecd9e120c4
Ellanki <sellanki@mcw.edu>
2019-10-30 11:41:54 -0500

    changes made in run.sh

--- src/main/dist/run.sh
+++ src/main/dist/run.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 . /etc/profile
-
 APPNAME=EnsemblDataPipeline
 APPDIR=/home/rgddata/pipelines/$APPNAME
 SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
-EMAILLIST=selanki@mcw.edu
+EMAILLIST=sellanki@mcw.edu
 
 if [ "$SERVER" == "REED" ]; then
-  EMAILLIST=selanki@mcw.edu
+  EMAILLIST=sellanki@mcw.edu
 fi
-
 cd $APPDIR
-
 java -jar -Dspring.config=$APPDIR/../properties/default_db.xml \
     -Dlog4j.configuration=file://$APPDIR/properties/log4j.properties \
     -jar lib/$APPNAME.jar "$@" run.sh 2>&1
